da1bbee36391bbc2f042ce83fc4269be885de212
Liu Yanbo <yanbo.liu@ucarinc.com>
2018-07-03 10:47:36 +0800

    fix style

--- Router.js
+++ Router.js
@@ -4,14 +4,14 @@
  * date：
  */
 
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {
-    StyleSheet,
-    View,
-    Text,
-} from 'react-native';
+  StyleSheet,
+  View,
+  Text
+} from 'react-native'
 
-import App from './Pages/App';
+import App from './Pages/App'
 import ButtonPage from './Pages/ButtonPage'
 import ImagePage from './Pages/ImagePage'
 import TimerPage from './Pages/TimerPage'
@@ -19,73 +19,72 @@ import InputPage from './Pages/InputPage'
 import WidgetPage from './Pages/WidgetPage'
 import AlertPage from './Pages/AlertPage'
 import LayoutPage from './Pages/LayoutPage'
-import {createBottomTabNavigator, createStackNavigator} from 'react-navigation';
-import TabPage from "./Pages/TabPage";
+import {createBottomTabNavigator, createStackNavigator} from 'react-navigation'
+import TabPage from './Pages/TabPage'
 
 class HomeScreen extends Component {
-    // static navigationOptions = {
-    //     // title: 'HomeScreen',
-    //     headerTitle: 'Home',
-    // };
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Home!</Text>
-            </View>
-        );
-    }
+  // static navigationOptions = {
+  //     // title: 'HomeScreen',
+  //     headerTitle: 'Home',
+  // };
+  render () {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    )
+  }
 }
 
 class SettingsScreen extends Component {
-    // static navigationOptions = {
-    //     title: 'SettingsScreen',
-    // };
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Settings!</Text>
-            </View>
-        );
-    }
+  // static navigationOptions = {
+  //     title: 'SettingsScreen',
+  // };
+  render () {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    )
+  }
 }
 
-
 export default createStackNavigator({
-    Home: {
-        screen: App,
-    },
-    Button: {
-        screen: ButtonPage,
-    },
-    Image: {
-        screen: ImagePage
-    },
-    Timer: {
-        screen: TimerPage
-    },
-    Input: {
-        screen: InputPage
-    },
-    Widget: {
-        screen: WidgetPage
-    },
-    Alert: {
-        screen: AlertPage
-    },
-    Layout: {
-        screen: LayoutPage
-    },
-    Tab: {
-        screen: createBottomTabNavigator({
-            Home: HomeScreen,
-            Settings: SettingsScreen,
-        }),
-        navigationOptions: ({navigation}) => ({
-            title: 'Tab',
-        }),
-    },
+  Home: {
+    screen: App
+  },
+  Button: {
+    screen: ButtonPage
+  },
+  Image: {
+    screen: ImagePage
+  },
+  Timer: {
+    screen: TimerPage
+  },
+  Input: {
+    screen: InputPage
+  },
+  Widget: {
+    screen: WidgetPage
+  },
+  Alert: {
+    screen: AlertPage
+  },
+  Layout: {
+    screen: LayoutPage
+  },
+  Tab: {
+    screen: createBottomTabNavigator({
+      Home: HomeScreen,
+      Settings: SettingsScreen
+    }),
+    navigationOptions: ({navigation}) => ({
+      title: 'Tab'
+    })
+  }
 }, {
 
-    initialRouteName: 'Home', // 设置默认的页面组件
+  initialRouteName: 'Home' // 设置默认的页面组件
 
-});
+})
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { AppRegistry } from 'react-native';
-import Router from './Router';
-import { YellowBox } from 'react-native';
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+import { AppRegistry } from 'react-native'
+import Router from './Router'
+import { YellowBox } from 'react-native'
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
 
-AppRegistry.registerComponent('RNPrac', () => Router);
+AppRegistry.registerComponent('RNPrac', () => Router)
